c89a08c77da557b9bc123744d851f64c47c7abfd
Justin Chuc <justinchuc@gmail.com>
2020-10-01 11:58:07 -0600

    Bootstrap Grid Part 1

---
+++ popper.js
@@ -0,0 +1,15 @@
+<body>
+<!-- jQuery first, then Popper.js, then Bootstrap JS. -->
+    <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+
+<p id="demo">Hi.</p>
+
+<script>
+document.
+("demo").innerHTML = "Hello World!";
+</script>
+
+</body>
